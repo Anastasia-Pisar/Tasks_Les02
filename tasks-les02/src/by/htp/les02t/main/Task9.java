7f914998408fc9270104453ff87a781d34b126a9
Anastasia <68652485+Anastasia-Pisar@users.noreply.github.com>
2020-08-14 17:38:06 +0300

    Update Task9.java

--- tasks-les02/src/by/htp/les02t/main/Task9.java
+++ tasks-les02/src/by/htp/les02t/main/Task9.java
@@ -8,12 +8,13 @@ public class Task9 {
 		double f;
 		
 		if(x <= 3) {
+			
 			f = Math.pow(x, 2) - 3 * x + 9;
-		}
-		else {
+			
+		} else {
+			
 			f = 1 / (Math.pow(x, 3) + 6);
 		}
 		System.out.println(f);
 	}
-
 }
